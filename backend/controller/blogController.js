eb6279906b638d6eee7b7545ab1db571649ff2b5
ktyjapz <katanyoo8945@gmail.com>
2023-12-12 23:52:28 +0700

    validate data

--- backend/controller/blogController.js
+++ backend/controller/blogController.js
@@ -3,6 +3,14 @@ const slugify = require('slugify');
 exports.create = (req,res) => {
     const { title, content, author } = req.body;
     const slug = slugify(title);
+
+    switch(true){
+        case !title:
+            return res.status(400).json({message: 'Please enter a title'});
+        case !content:
+            return res.status(400).json({message: 'Please enter a content'});
+    }
+    
     res.json({
         data: {
             title, content, author,slug
